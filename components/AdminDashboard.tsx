bcfb72f547743f063c5b8a8c50f9f5add1696ea3
tasneem mohamad <tmoh4218@gmail.com>
2023-11-15 20:56:13 +0300

    auth + tabs

--- components/AdminDashboard.tsx
+++ components/AdminDashboard.tsx
@@ -1,25 +1,39 @@
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import Icon from  'react-native-vector-icons/FontAwesome';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
   useColorScheme,
   View,
 } from 'react-native';
-
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Stack =createNativeStackNavigator();
+const Tab = createMaterialBottomTabNavigator();
+
+const HomeScreen =()=>{
+return(
+  <Text>Home</Text>
+)
+}
+
+const StudentsScreen =()=>{
+  return(
+    <Text>Home</Text>
+  )}
 
 function App(): JSX.Element {
   
   return (
   <>
-  <Text>Admin</Text>
+    <Tab.Navigator barStyle={{backgroundColor:'white',}} activeColor='#abdcfa' >
+        <Tab.Screen name='Home'component={HomeScreen} options={{tabBarIcon:(props)=>(<Icon name="home" size={20} color={'#abdcfa'} />)}}/>
+        <Tab.Screen name='Student'component={StudentsScreen} options={{tabBarIcon:(props)=>(<Icon name="users" size={20}color={'#abdcfa'}/>)}}/>
+    </Tab.Navigator> 
   </>
   );
 }
