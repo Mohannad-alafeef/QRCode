1d250d341c44d3a8e9de9c67b2e06d712805d108
Mohannad <90407906+Mohannad-alafeef@users.noreply.github.com>
2023-11-16 21:23:17 +0300

    declare admin components

---
+++ components/AdminComponents/StudentStack.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+function StudentStack() {
+  return <Text>Student Stack</Text>;
+}
+
+export default StudentStack;
--- components/AdminDashboard.tsx
+++ components/AdminDashboard.tsx
@@ -1,45 +1,57 @@
-
 import React from 'react';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import Icon from  'react-native-vector-icons/FontAwesome';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 
-import {
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {StyleSheet, Text, useColorScheme, View} from 'react-native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import CourseStack from './AdminComponents/CourseStack';
+import StudentStack from './AdminComponents/StudentStack';
+import AdminProfile from './AdminComponents/AdminProfile';
 
-const Stack =createNativeStackNavigator();
+const Stack = createNativeStackNavigator();
 const Tab = createMaterialBottomTabNavigator();
 
-const HomeScreen =()=>{
-return(
-  <Text>Home</Text>
-)
-}
-
-const StudentsScreen =()=>{
-  return(
-    <Text>Home</Text>
-  )}
-
 function App(): JSX.Element {
-  
   return (
-  <>
-    <Tab.Navigator barStyle={{backgroundColor:'white',}} activeColor='#abdcfa' >
-        <Tab.Screen name='Home'component={HomeScreen} options={{tabBarIcon:(props)=>(<Icon name="home" size={20} color={'#abdcfa'} />)}}/>
-        <Tab.Screen name='Student'component={StudentsScreen} options={{tabBarIcon:(props)=>(<Icon name="users" size={20}color={'#abdcfa'}/>)}}/>
-    </Tab.Navigator> 
-  </>
+    <>
+      <Tab.Navigator
+        barStyle={{backgroundColor: 'white'}}
+        activeColor="#abdcfa">
+        <Tab.Screen
+          name="CoursesStack"
+          component={CourseStack}
+          options={{
+            tabBarIcon: props => (
+              <Icon name="book" size={20} color={'#abdcfa'} />
+            ),
+            title: 'Courses',
+          }}
+        />
+        <Tab.Screen
+          name="StudentStack"
+          component={StudentStack}
+          options={{
+            tabBarIcon: props => (
+              <Icon name="users" size={20} color={'#abdcfa'} />
+            ),
+            title: 'Students',
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={AdminProfile}
+          options={{
+            tabBarIcon: props => (
+              <Icon name="user" size={20} color={'#abdcfa'} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
- 
-});
+const styles = StyleSheet.create({});
 
 export default App;
